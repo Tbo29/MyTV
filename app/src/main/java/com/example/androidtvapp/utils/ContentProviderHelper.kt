629bc8e7970c9fa8e62420fced124236af072f13
Tbo29 <thebigone2903@gmail.com>
2026-04-05 09:56:34 +0200

    Change return types of channel-related functions

--- app/src/main/java/com/example/androidtvapp/utils/ContentProviderHelper.kt
+++ app/src/main/java/com/example/androidtvapp/utils/ContentProviderHelper.kt
@@ -1,21 +1,21 @@
-package com.example.androidtvapp.utils
+implementationResolvere.androidtvapp.utils
 
 import android.content.ContentResolver
 import android.media.tv.TvContract
 import android.net.Uri
 
 object ContentProviderHelper {
-    fun getChannels(contentResolver: ContentResolver): List<Any> {
+    fun getChannels(contentResolver: ContentResolver): List<Channel> {
         // Your implementation
         return emptyList()
     }
     
-    fun getPreviewPrograms(contentResolver: ContentResolver): List<Any> {
+    fun getPreviewPrograms(contentResolver: ContentResolver): List<PreviewProgram> {
         // Your implementation
         return emptyList()
     }
     
-    fun getWatchNextPrograms(contentResolver: ContentResolver): List<Any> {
+    fun getWatchNextPrograms(contentResolver: ContentResolver): List<WatchNextProgram> {
         // Your implementation
         return emptyList()
     }
